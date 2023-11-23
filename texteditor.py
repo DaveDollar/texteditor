c5f8918b9b6eec6c2a2868017c02aa6027c3905d
DaveDollar <illes.david.2006@gmail.com>
2023-11-23 09:24:33 +0100

    python

--- texteditor.py
+++ texteditor.py
@@ -3,9 +3,40 @@ from tkinter import *
 root = Tk()
 root.title('Text Editor')
 root.geometry('1000x600')
-root.resizable(False, False)
 
-text = Text(root, )
-text.grid(row=0, column=2)
+root.configure(bg='#313031')
+
+menubar = Menu(root)
+root.config(menu=menubar)
+file_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(
+    label='Fájl',
+    menu=file_menu
+)
+
+file_menu.add_command(
+    label='Megnyitás'
+)
+
+file_menu.add_command(
+    label='Mentés'
+)
+
+file_menu.add_command(
+    label='Kilépés',
+    command=root.destroy
+)
+
+
+sbar = Scrollbar(root, orient='vertical')
+sbar.pack(side=RIGHT, fill='y')
+
+text = Text(root, wrap=WORD, height=100, width=110, yscrollcommand=sbar.set)
+sbar.config(command=text.yview)
+text.pack()
+
+
+
+
 
 root.mainloop()
